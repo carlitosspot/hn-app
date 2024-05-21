16f94cebcecb24f6f2297313ddb2d0a1377944c2
Carlos Renoj <renojpa@gmail.com>
2024-05-21 12:13:06 -0400

    removes temp logging

--- src/api/hnApi.ts
+++ src/api/hnApi.ts
@@ -12,7 +12,6 @@ export interface IStory {
 }
 
 export const fetchTopStories = async (): Promise<number[]> => {
-  console.log('xxxxxx', import.meta.env.MODE, import.meta.env.VITE_HN_BASE_API);
   const response = await fetch(`${BASE_URL}/topstories.json`);
   const storyIds = await response.json();
   return storyIds;
